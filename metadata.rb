1a28ce95edd4181fbdd7e75b75ec82f9fe3be371
mattray <matthewhray@gmail.com>
2012-11-01 11:04:50 -0500

    versioned to match OpenStack releases

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "The OpenStack Compute service Nova."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "5.0.0"
+version          "2012.1.0"
 
 recipe		  "api-ec2", ""
 recipe		  "api-metadata", ""
